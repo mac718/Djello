c5e9e069d62c8c0e28b7ce51ecc31cdab021d7af
mac718 <mac718@gmail.com>
2020-02-09 13:09:09 -0800

    set up passport

--- server.js
+++ server.js
@@ -4,14 +4,60 @@ require('isomorphic-fetch');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+
 
 app.use((req, res, next) => {
   if(mongoose.connection.readyState) {
     next();
   } else {
-    require('./mongo').then(() => next());
+    require('./mongo')().then(() => next());
   }
-})
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(
+  expressSession({
+    secret: process.env.secret || 'fiona',
+    saveUninitialized: false,
+    resave: false
+  }))
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+const User = require('./models/User');
+const LocalStrategy = require('passport-local').Strategy
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username }, function(err, user) {
+      console.log(user)
+      if(err) return done(err);
+      if(!user || !user.validPassword(password)) {
+        return done(null, false, { message: 'Invalid username/password' });
+      }
+    })
+    return done(null, user);
+  });
+);
+
+passport.serializeUser((user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser((user, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+}); 
 
 app.set('port', (process.env.PORT || 3001));
 
@@ -35,6 +81,17 @@ function parseJSON(response) {
 
 app.get('/', (req, res) => {
   res.send('Hello');
+})
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return next(err); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.status(200).send;
+    });
+  })(req, res, next);
 })
 
 function errorHandler(err, req, res, next) {
