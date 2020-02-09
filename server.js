3a39c9e57f480eb832f2a459bc0373bc75d4805e
mac718 <mac718@gmail.com>
2020-02-08 17:03:26 -0800

    Initial server.js setup

---
+++ server.js
@@ -0,0 +1,41 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+function checkStatus(response) {
+  if(!response.ok) {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error
+  }
+
+  return response;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+})
+
+function errorHandler(err, req, res, next) {
+  console.error('Error: ', err.stack);
+  res.status(err.response ? err.response.status : 500);
+  res.json({ error: err.message });
+}
+
+app.use(errorHandler);
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+})
