4ce125399e70e513c5457c0d20a43204189d9a72
mac718 <mac718@gmail.com>
2020-04-20 14:21:30 -0700

    display 'no members yet' in cardMemberList if no members yet

--- client/src/components/CardMemberList.js
+++ client/src/components/CardMemberList.js
@@ -11,16 +11,11 @@ const CardMemberList = ({ members }) => {
         >
           Members
         </span>
-        {/* <span
-          className="icon"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-        >
-          <i className="fas fa-users"></i>
-        </span> */}
       </div>
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">{members}</div>
+        <div className="dropdown-content">
+          {members.length > 0 ? members : 'no members yet'}
+        </div>
       </div>
     </div>
   )
